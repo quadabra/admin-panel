a31246c775d41be9de0eecd5570f36d79981f089
quadabra <quadabra@gmail.com>
2018-12-25 17:18:14 +0300

    product details 0.2.1 some inputs and hardcode model

--- src/app/content/catalog/catalog-products/product-details/product-details.component.ts
+++ src/app/content/catalog/catalog-products/product-details/product-details.component.ts
@@ -10,16 +10,54 @@ import {IProduct} from '../../../../_model/interface/product';
 export class ProductDetailsComponent implements OnInit {
   langs = ['En', 'Ru', 'De', 'Fr'];
   dataSource: IProduct;
-  brands = [
-    {value: 'sso-0', viewValue: 'SSO'},
-    {value: 'ana-1', viewValue: 'ANA'},
-    {value: 'tech-2', viewValue: 'Techincome'}
+
+  seoOptions = ['Link', 'Title', 'Keyword'];
+
+  textfields = [
+    {value: 'weight', viewValue: 'Weight'},
+    {value: '', viewValue: 'WH Position'},
+    {value: 'model', viewValue: 'Mail Model'},
+    {value: 'code', viewValue: 'Vendor Code'},
+    {value: 'priority', viewValue: 'Priority'},
   ];
-  manufacrurers = [
-    {value: 'sso-0', viewValue: 'SSO'},
-    {value: 'ana-1', viewValue: 'ANA'},
-    {value: 'tech-2', viewValue: 'Techincome'}
-  ];
+
+  brands = {
+    groupName: 'Brand',
+    type: 'select',
+    options: [
+      {
+        fieldName: 'Brand',
+        list: [
+          {value: 'sso-0', viewValue: 'SSO'},
+          {value: 'ana-1', viewValue: 'ANA'},
+          {value: 'tech-2', viewValue: 'Techincome'}
+        ]
+      },
+      {
+        fieldName: 'Manufacturers',
+        list: [
+          {value: 'sso-0', viewValue: 'SSO1'},
+          {value: 'ana-1', viewValue: 'ANA2'},
+          {value: 'tech-2', viewValue: 'Techincome3'}
+        ]
+      },
+    ]
+  };
+
+  prices = {
+    groupName: 'Prices',
+    type: 'number',
+    options: [
+      {
+        fieldName: 'Price Buy',
+        value: '2000'
+      },
+      {
+        fieldName: 'Price Sell',
+        value: '3000'
+      }
+    ]
+  };
 
   constructor(private route: ActivatedRoute) {
   }
