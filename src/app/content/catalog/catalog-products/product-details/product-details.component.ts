8e6042868d818aacd21e50605275ae50d711a061
quadabra <quadabra@gmail.com>
2018-12-25 15:08:40 +0300

    product details 0.2

--- src/app/content/catalog/catalog-products/product-details/product-details.component.ts
+++ src/app/content/catalog/catalog-products/product-details/product-details.component.ts
@@ -8,8 +8,18 @@ import {IProduct} from '../../../../_model/interface/product';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-
+  langs = ['En', 'Ru', 'De', 'Fr'];
   dataSource: IProduct;
+  brands = [
+    {value: 'sso-0', viewValue: 'SSO'},
+    {value: 'ana-1', viewValue: 'ANA'},
+    {value: 'tech-2', viewValue: 'Techincome'}
+  ];
+  manufacrurers = [
+    {value: 'sso-0', viewValue: 'SSO'},
+    {value: 'ana-1', viewValue: 'ANA'},
+    {value: 'tech-2', viewValue: 'Techincome'}
+  ];
 
   constructor(private route: ActivatedRoute) {
   }
