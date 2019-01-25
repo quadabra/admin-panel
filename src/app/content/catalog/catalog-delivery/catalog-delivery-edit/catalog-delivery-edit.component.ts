06d1007614b5f9bc830f8693f2dd9886184207ef
quadabra <quadabra@gmail.com>
2019-01-25 14:42:04 +0300

    delivery edit zones

--- src/app/content/catalog/catalog-delivery/catalog-delivery-edit/catalog-delivery-edit.component.ts
+++ src/app/content/catalog/catalog-delivery/catalog-delivery-edit/catalog-delivery-edit.component.ts
@@ -13,6 +13,172 @@ export class CatalogDeliveryEditComponent implements OnInit {
   weightTypes = ['Regular', 'Volume'];
   ShowRestricted: string;
   ShowRestrictedItems = ['Yes', 'No'];
+  displayedColumns = ['weight', 'cost'];
+  zones = [
+    {
+      country_list: ['germany', 'france', 'italy'],
+      prices: [
+        {
+          weight: 0.1,
+          cost: 10
+        },
+        {
+          weight: 0.2,
+          cost: 20
+        },
+        {
+          weight: 0.3,
+          cost: 130
+        },
+        {
+          weight: 0.41,
+          cost: 140
+        },
+        {
+          weight: 0.1,
+          cost: 10
+        },
+        {
+          weight: 0.2,
+          cost: 20
+        },
+        {
+          weight: 0.3,
+          cost: 130
+        },
+        {
+          weight: 0.41,
+          cost: 140
+        }
+      ]
+    },
+    {
+      country_list: ['usa', 'limpopo', 'kanada'],
+      prices: [
+        {
+          weight: 0.1,
+          cost: 10
+        },
+        {
+          weight: 0.2,
+          cost: 20
+        },
+        {
+          weight: 0.3,
+          cost: 130
+        },
+        {
+          weight: 0.41,
+          cost: 140
+        }
+      ]
+    },
+    {
+      country_list: ['germany', 'france', 'italy'],
+      prices: [
+        {
+          weight: 0.1,
+          cost: 10
+        },
+        {
+          weight: 0.2,
+          cost: 20
+        },
+        {
+          weight: 0.3,
+          cost: 130
+        },
+        {
+          weight: 0.41,
+          cost: 140
+        }
+      ]
+    },
+    {
+      country_list: ['germany', 'france', 'italy'],
+      prices: [
+        {
+          weight: 0.1,
+          cost: 10
+        },
+        {
+          weight: 0.2,
+          cost: 20
+        },
+        {
+          weight: 0.3,
+          cost: 130
+        },
+        {
+          weight: 0.41,
+          cost: 140
+        }
+      ]
+    },
+    {
+      country_list: ['germany', 'france', 'italy'],
+      prices: [
+        {
+          weight: 0.1,
+          cost: 10
+        },
+        {
+          weight: 0.2,
+          cost: 20
+        },
+        {
+          weight: 0.3,
+          cost: 130
+        },
+        {
+          weight: 0.41,
+          cost: 140
+        }
+      ]
+    },
+    {
+      country_list: ['germany', 'france', 'italy'],
+      prices: [
+        {
+          weight: 0.1,
+          cost: 10
+        },
+        {
+          weight: 0.2,
+          cost: 20
+        },
+        {
+          weight: 0.3,
+          cost: 130
+        },
+        {
+          weight: 0.41,
+          cost: 140
+        }
+      ]
+    },
+    {
+      country_list: ['germany', 'france', 'italy'],
+      prices: [
+        {
+          weight: 0.1,
+          cost: 10
+        },
+        {
+          weight: 0.2,
+          cost: 20
+        },
+        {
+          weight: 0.3,
+          cost: 130
+        },
+        {
+          weight: 0.41,
+          cost: 140
+        }
+      ]
+    }
+  ];
 
   constructor(private fb: FormBuilder) {
     this.deliveryForm = this.fb.group({
