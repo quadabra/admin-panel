6b546d96a948bb3e5d7344a1fbf663b8d434c4b6
quadabra <quadabra@gmail.com>
2019-01-29 21:02:57 +0300

    incoming-edit

--- src/app/content/wh/wh-incoming/wh-incoming-edit/wh-incoming-edit.component.ts
+++ src/app/content/wh/wh-incoming/wh-incoming-edit/wh-incoming-edit.component.ts
@@ -6,7 +6,94 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wh-incoming-edit.component.css']
 })
 export class WhIncomingEditComponent implements OnInit {
-
+  manufacturers = ['SSO', 'ANA', 'Techincome'];
+  displayedColumns = ['name', 'options', 'manufacturer', 'count', 'image', 'action'];
+  items = ['item1', 'item2', 'item3'];
+  dataSource = [
+    {
+      name: 'item1',
+      options: [{type: 'color', value: 'color1'}, {type: 'size', value: 'size1'}, {type: 'height', value: 'height1'}],
+      manufacturer: 'ANA',
+      quantity: 16,
+      image: 'no image'
+    },
+    {
+      name: 'item2',
+      options: [{type: 'color', value: 'color1'}, {type: 'size', value: 'size1'}, {type: 'height', value: 'height1'}],
+      manufacturer: 'ANA',
+      quantity: 16,
+      image: 'no image'
+    },
+    {
+      name: 'item3',
+      options: [{type: 'color', value: 'color1'}, {type: 'size', value: 'size1'}, {type: 'height', value: 'height1'}],
+      manufacturer: 'ANA',
+      quantity: 16,
+      image: 'no image'
+    },
+    {
+      name: 'item1',
+      options: [{type: 'color', value: 'color1'}, {type: 'size', value: 'size1'}, {type: 'height', value: 'height1'}],
+      manufacturer: 'ANA',
+      quantity: 16,
+      image: 'no image'
+    },
+    {
+      name: 'item2',
+      options: [{type: 'color', value: 'color1'}, {type: 'size', value: 'size1'}, {type: 'height', value: 'height1'}],
+      manufacturer: 'ANA',
+      quantity: 16,
+      image: 'no image'
+    },
+    {
+      name: 'item3',
+      options: [{type: 'color', value: 'color1'}, {type: 'size', value: 'size1'}, {type: 'height', value: 'height1'}],
+      manufacturer: 'ANA',
+      quantity: 16,
+      image: 'no image'
+    },{
+      name: 'item1',
+      options: [{type: 'color', value: 'color1'}, {type: 'size', value: 'size1'}, {type: 'height', value: 'height1'}],
+      manufacturer: 'ANA',
+      quantity: 16,
+      image: 'no image'
+    },
+    {
+      name: 'item2',
+      options: [{type: 'color', value: 'color1'}, {type: 'size', value: 'size1'}, {type: 'height', value: 'height1'}],
+      manufacturer: 'ANA',
+      quantity: 16,
+      image: 'no image'
+    },
+    {
+      name: 'item3',
+      options: [{type: 'color', value: 'color1'}, {type: 'size', value: 'size1'}, {type: 'height', value: 'height1'}],
+      manufacturer: 'ANA',
+      quantity: 16,
+      image: 'no image'
+    },
+    {
+      name: 'item1',
+      options: [{type: 'color', value: 'color1'}, {type: 'size', value: 'size1'}, {type: 'height', value: 'height1'}],
+      manufacturer: 'ANA',
+      quantity: 16,
+      image: 'no image'
+    },
+    {
+      name: 'item2',
+      options: [{type: 'color', value: 'color1'}, {type: 'size', value: 'size1'}, {type: 'height', value: 'height1'}],
+      manufacturer: 'ANA',
+      quantity: 16,
+      image: 'no image'
+    },
+    {
+      name: 'item3',
+      options: [{type: 'color', value: 'color1'}, {type: 'size', value: 'size1'}, {type: 'height', value: 'height1'}],
+      manufacturer: 'ANA',
+      quantity: 16,
+      image: 'no image'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
