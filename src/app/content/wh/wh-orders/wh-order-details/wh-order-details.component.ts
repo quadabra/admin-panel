e1cc88712da93506ef2c392efdfd82d5eb7c8eef
quadabra <quadabra@gmail.com>
2019-02-01 21:06:29 +0300

    added wh order details

---
+++ src/app/content/wh/wh-orders/wh-order-details/wh-order-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wh-order-details',
+  templateUrl: './wh-order-details.component.html',
+  styleUrls: ['./wh-order-details.component.css']
+})
+export class WhOrderDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
