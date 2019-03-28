79517d16d13e191efe3ea61ac953b4f301cb5c0d
quadabra <quadabra@gmail.com>
2019-03-28 18:51:57 +0300

    +assembly-active rmk

--- src/app/content/catalog/assembly/assembly-active/assembly-active.component.ts
+++ src/app/content/catalog/assembly/assembly-active/assembly-active.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-assembly-active',
@@ -8,12 +11,13 @@ import { Component, OnInit } from '@angular/core';
 export class AssemblyActiveComponent implements OnInit {
   objectKeys = Object.keys;
   displayedColumns: string[] = ['image', 'name', 'manufacturer', 'options', 'check'];
-  isLargeImages = false;
+  isLargeImages = true;
 
   dataSource = [
     {
       image: 'azaz',
       name: 'razraz',
+      nameRu: 'азазаза',
       manufacturer: 'akakaka',
       options: {
         size: '32',
@@ -24,6 +28,7 @@ export class AssemblyActiveComponent implements OnInit {
     {
       image: 'azaz',
       name: 'razraz',
+      nameRu: 'азазаза',
       manufacturer: 'akakaka',
       options: {
         size: '32',
@@ -34,6 +39,7 @@ export class AssemblyActiveComponent implements OnInit {
     {
       image: 'azaz',
       name: 'razraz',
+      nameRu: 'азазаза',
       manufacturer: 'akakaka',
       options: {
         size: '32',
@@ -44,6 +50,7 @@ export class AssemblyActiveComponent implements OnInit {
     {
       image: 'azaz',
       name: 'razraz',
+      nameRu: 'азазаза',
       manufacturer: 'akakaka',
       options: {
         size: '32',
@@ -52,10 +59,18 @@ export class AssemblyActiveComponent implements OnInit {
       }
     },
   ];
-  constructor() { }
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+  }
 
   ngOnInit() {
   }
+
   toggleImageSize(): void {
     this.isLargeImages = !this.isLargeImages;
   }
