ea03544d91e2f133cb8453677cdd4bd0f67b1c1a
quadabra <quadabra@gmail.com>
2019-04-09 02:23:38 +0300

    +assembly-active ++slider

--- src/app/content/catalog/payments/payments.module.ts
+++ src/app/content/catalog/payments/payments.module.ts
@@ -3,6 +3,7 @@ import {CommonModule} from '@angular/common';
 import {PaymentsRoutingModule} from './payments-routing.module';
 import {MaterialModule} from '../../../material.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {SharedModule} from '../../../_shared/shared.module';
 
 //  Components
 import {PaymentsComponent} from './payments.component';
@@ -15,6 +16,7 @@ const modules = [
   MaterialModule,
   FormsModule,
   ReactiveFormsModule,
+  SharedModule,
 ];
 
 const components = [
